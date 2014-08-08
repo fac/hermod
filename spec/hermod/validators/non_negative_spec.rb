edb15246baf5a639fcc962e11bc1b48a1466af01
Harry Mills <harry@haeg.in>
2014-08-08 12:23:47 +0100

    Add tests for non negative validator

---
+++ spec/hermod/validators/non_negative_spec.rb
@@ -0,0 +1,24 @@
+require "minitest_helper"
+
+module Hermod
+  module Validators
+    describe NonNegative do
+      subject do
+        NonNegative.new
+      end
+
+      it "allows positive values" do
+        subject.valid?(1, {}).must_equal true
+      end
+
+      it "allows zero values" do
+        subject.valid?(0, {}).must_equal true
+      end
+
+      it "raises an error for negative values" do
+        ex = proc { subject.valid?(-1, {}) }.must_raise InvalidInputError
+        ex.message.must_equal "cannot be negative"
+      end
+    end
+  end
+end

3b5ed2be3045811fe6a372ce725e014b48db550b
Harry Mills <harry@haeg.in>
2014-08-08 12:00:26 +0100

    Add tests for AllowedValues validator

--- lib/hermod/validators/allowed_values.rb
+++ lib/hermod/validators/allowed_values.rb
@@ -18,7 +18,8 @@ module Hermod
       end
 
       def message
-        "must be one of #{allowed_values.to_sentence}, not #{value}"
+        list_of_values = allowed_values.to_sentence(last_word_connector: ", or ", two_words_connector: " or ")
+        "must be one of #{list_of_values}, not #{value}"
       end
     end
   end
